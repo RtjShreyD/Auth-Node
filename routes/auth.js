daf48a7871b35beacbc6cbb268380951906e25ab
rtjshreyd <shreyanshece1041@gmail.com>
2021-06-30 03:11:04 +0530

    User register, email validation and request data validation using hapiJoi lib

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,46 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+// validation
+const { registerValidation } = require("../validation");
+
+// encryption lib
+const bcrypt = require("bcrypt");
+
 router.post("/register", async (req, res) => {
-  const user = new User({
+    // validate the user
+    const { error } = registerValidation(req.body);
+    
+    if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+    }  
+
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    
+    if (isEmailExist) {
+        return res.status(400).json({ error: "Email already exists" });
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password,
   });
-  try {
+
+  try 
+  {
     const savedUser = await user.save();
-    res.json({ error: null, data: savedUser });
-  } catch (error) {
+    res.json({ error: null, data: { userId: savedUser._id } });
+  } 
+  catch (error) 
+  {
     res.status(400).json({ error });
   }
+
 });
 
 module.exports = router;
