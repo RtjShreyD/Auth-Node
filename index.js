6e902f8415e6714a74599c6b23b4257841e1763a
rtjshreyd <shreyanshece1041@gmail.com>
2021-06-30 02:09:32 +0530

    Basic connection to mongo Db

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// connect to db
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log("connected to db"));
+
 // import routes
 const authRoutes = require("./routes/auth");
 
